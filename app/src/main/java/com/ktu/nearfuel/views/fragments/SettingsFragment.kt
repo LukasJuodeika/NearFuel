329f3f3608cff8f8e8029ab8d7efe0acb3f4c955
Lukas Juodeika <lksjuodeika@gmail.com>
2019-03-18 21:04:24 +0200

    Use MVP for SettingsFragment

--- app/src/main/java/com/ktu/nearfuel/views/fragments/SettingsFragment.kt
+++ app/src/main/java/com/ktu/nearfuel/views/fragments/SettingsFragment.kt
@@ -5,12 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.ktu.components.contracts.SettingsContract
+import com.ktu.components.presenters.SettingsPresenter
 import com.ktu.nearfuel.R
 
-class SettingsFragment : Fragment()
+class SettingsFragment : Fragment(), SettingsContract.View
 {
+    private lateinit var presenter: SettingsContract.Presenter
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView = inflater.inflate(R.layout.settings_fragment,null)
+        presenter = SettingsPresenter(this)
 
         return rootView
     }
---
+++ components/src/main/java/com/ktu/components/presenters/SettingsPresenter.kt
@@ -0,0 +1,8 @@
+package com.ktu.components.presenters
+
+import com.ktu.components.contracts.SettingsContract
+
+class SettingsPresenter(val view: SettingsContract.View): SettingsContract.Presenter
+{
+
+}
