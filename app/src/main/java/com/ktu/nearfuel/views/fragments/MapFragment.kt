d538d12dbecd7e10471558bf1d8cee17fd9a6147
Lukas Juodeika <lksjuodeika@gmail.com>
2019-03-18 16:38:28 +0200

    Enable drawer only in map fragment

--- app/src/main/java/com/ktu/nearfuel/views/fragments/MapFragment.kt
+++ app/src/main/java/com/ktu/nearfuel/views/fragments/MapFragment.kt
@@ -1,12 +1,15 @@
 package com.ktu.nearfuel.views.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.ktu.nearfuel.R
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.map_fragment.view.*
 
 class MapFragment : Fragment()
@@ -23,5 +26,17 @@ class MapFragment : Fragment()
         view.add_gas_station.setOnClickListener {
             view.findNavController().navigate(R.id.action_mapFragment_to_addStationFragment)
         }
+    }
+
+    override fun onResume() {
+        activity!!.drawer_layout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+        super.onResume()
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        activity!!.drawer_layout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+
     }
 }
