c65b66d3eb8c9a967d73c80bc8a8a3eb8f2c2f6c
Lukas Juodeika <lksjuodeika@gmail.com>
2019-03-18 16:20:24 +0200

    Implement viewpager with tabs

--- app/src/main/java/com/ktu/nearfuel/views/fragments/GasStationListFragment.kt
+++ app/src/main/java/com/ktu/nearfuel/views/fragments/GasStationListFragment.kt
@@ -6,15 +6,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.ktu.nearfuel.R
+import com.ktu.nearfuel.views.ViewPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.gas_station_list_view.view.*
 
 class GasStationListFragment : Fragment()
 {
+    lateinit var pagerAdapter: ViewPagerAdapter
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView = inflater.inflate(R.layout.gas_station_list_view,null)
+
+        pagerAdapter = ViewPagerAdapter(activity!!.supportFragmentManager)
+        rootView.viewpager.adapter = pagerAdapter
+        rootView.tablayout.setupWithViewPager(rootView.viewpager)
+
+
         return rootView
     }
 
