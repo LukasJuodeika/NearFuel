47a40ee5fc0ca7c019d0360abac3897c1fb83b84
Lukas Juodeika <lksjuodeika@gmail.com>
2019-03-18 14:17:23 +0200

    Add navigation drawer

--- app/src/main/java/com/ktu/nearfuel/views/activities/MainActivity.kt
+++ app/src/main/java/com/ktu/nearfuel/views/activities/MainActivity.kt
@@ -2,13 +2,77 @@ package com.ktu.nearfuel.views.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.core.view.GravityCompat
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.navigation.NavigationView
 import com.ktu.nearfuel.R
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
        // setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupNavigation()
     }
+
+
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+
+        p0.isChecked = true
+
+        drawer_layout.closeDrawers()
+
+        val id = p0.itemId
+
+        when (id) {
+            R.id.gas_station_list ->
+            navController.navigate(R.id.action_mapFragment_to_gasStationListFragment)
+
+            //R.id.help ->
+            //navController.navigate(R.id.secondFragment)
+
+            //R.id.settings->
+            //navController.navigate(R.id.thirdFragment)
+        }
+        return true
+    }
+
+    private fun setupNavigation() {
+
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        NavigationUI.setupActionBarWithNavController(this, navController, drawer_layout)
+
+        NavigationUI.setupWithNavController(navigationView, navController)
+
+        navigationView.setNavigationItemSelectedListener(this)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(Navigation.findNavController(this, R.id.nav_host_fragment), drawer_layout)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+
+    }
+
+
 }
