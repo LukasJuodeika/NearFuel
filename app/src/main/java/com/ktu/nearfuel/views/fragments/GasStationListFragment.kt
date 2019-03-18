12041fdee6e933eff5adea1934986bf6fd5e3ffc
Lukas Juodeika <lksjuodeika@gmail.com>
2019-03-18 15:14:24 +0200

    Update app colors and styles

--- app/src/main/java/com/ktu/nearfuel/views/fragments/GasStationListFragment.kt
+++ app/src/main/java/com/ktu/nearfuel/views/fragments/GasStationListFragment.kt
@@ -1,16 +1,35 @@
 package com.ktu.nearfuel.views.fragments
 
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.ktu.nearfuel.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class GasStationListFragment : Fragment()
 {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView = inflater.inflate(R.layout.gas_station_list_view,null)
         return rootView
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            activity!!.toolbar.elevation = 0F
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            activity!!.toolbar.elevation = 8F
+        }
+
     }
 }
