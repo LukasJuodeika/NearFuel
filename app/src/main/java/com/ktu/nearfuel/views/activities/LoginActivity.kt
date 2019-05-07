23e025130fe6281451d5e7912d4a8ce4a315d6c5
Ernestas <raudonisernestas@gmail.com>
2019-05-07 11:24:30 +0300

    Add login activity

---
+++ app/src/main/java/com/ktu/nearfuel/views/activities/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.ktu.nearfuel.views.activities
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.navigation.NavigationView
+
+class LoginActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
