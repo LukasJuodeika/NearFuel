c145ca9f78dcbc5e1603495d036fc9732d5c7c1b
Viktor Vostrikov <viktoras@idenfy.com>
2019-05-29 14:57:55 +0300

    null emits

--- app/src/main/java/com/ktu/nearfuel/views/StationInfoWindowAdapter.kt
+++ app/src/main/java/com/ktu/nearfuel/views/StationInfoWindowAdapter.kt
@@ -18,8 +18,19 @@ class StationInfoWindowAdapter(private val mContext: Context) : GoogleMap.InfoWi
         view.iw_title.text = station.title
         view.iw_address.text = station.address
         val resources = mContext.resources
+        if(station.diesel_price==null)
+        {
+            view.iw_diesel_price.text = resources.getString(R.string.iw_diesel, "No data")
+        }
+        else
         view.iw_diesel_price.text = resources.getString(R.string.iw_diesel, station.diesel_price)
+        if(station.fuel_price==null)
+            view.iw_fuel_price.text = resources.getString(R.string.iw_diesel, "No data")
+        else
         view.iw_fuel_price.text = resources.getString(R.string.iw_petrol, station.fuel_price)
+        if(station.gas_price==null)
+            view.iw_gas_price.text = resources.getString(R.string.iw_diesel, "No data")
+        else
         view.iw_gas_price.text = resources.getString(R.string.iw_gas, station.gas_price)
         view.iw_updated_at.text = resources.getString(R.string.iw_updated_at, station.updated_at)
     }
